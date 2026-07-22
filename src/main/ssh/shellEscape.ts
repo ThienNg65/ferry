ddfd55c0d12d20cdad68f0da6dc8c0a20087e661
ThienNg65 <nvtt1510@gmail.com>
2026-07-22 22:46:35 +0700

    chore: remove legacy electron source code

--- src/main/ssh/shellEscape.ts
+++
@@ -1,4 +0,0 @@
-/** Single-quotes a value for safe interpolation into a remote (POSIX) shell command. */
-export function shellEscape(value: string): string {
-  return `'${value.replace(/'/g, `'\\''`)}'`
-}
